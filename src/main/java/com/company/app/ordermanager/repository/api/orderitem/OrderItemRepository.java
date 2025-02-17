dfab3328edd6d118160a22d1130f460aef8cddfa
samuele ruffino <sruffino@sinelec.it>
2025-02-17 19:32:56 +0100

    feat(repository): add method to update order item status
    Added `updateStatus` method to update the status of an order item
    based on its ID, product ID, and order ID.

--- src/main/java/com/company/app/ordermanager/repository/api/orderitem/OrderItemRepository.java
+++ src/main/java/com/company/app/ordermanager/repository/api/orderitem/OrderItemRepository.java
@@ -1,9 +1,22 @@
 package com.company.app.ordermanager.repository.api.orderitem;
 
 import com.company.app.ordermanager.entity.orderitem.OrderItem;
+import com.company.app.ordermanager.entity.orderitem.OrderItemStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, UUID> {
+    @Modifying
+    @Query("UPDATE OrderItem oi SET oi.status = :status " +
+            "WHERE oi.id = :orderItemId " +
+            "AND oi.product.id = :productId " +
+            "AND oi.order.id = :orderId")
+    int updateStatus(@Param("orderItemId") UUID orderItemId,
+                     @Param("productId") UUID productId,
+                     @Param("orderId") UUID orderId,
+                     @Param("status") OrderItemStatus status);
 }
