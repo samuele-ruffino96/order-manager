ec0044556923354a0b24268e52cd9f5c2e4a7aec
samuele ruffino <sruffino@sinelec.it>
2025-02-17 09:26:23 +0100

    docs: add documentation for StreamNames enum
    Provide detailed descriptions for Redis stream names and their purposes.

--- src/main/java/com/company/app/ordermanager/redis/stream/common/StreamNames.java
+++ src/main/java/com/company/app/ordermanager/redis/stream/common/StreamNames.java
@@ -4,6 +4,17 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * Represents a collection of predefined Redis stream names used in the application for publishing
+ * and processing messages. Each constant in this enum represents a specific stream name tied to
+ * a distinct functionality such as stock updates, product updates, or order confirmations.
+ *
+ * <ul>
+ *   <li>{@link #STOCK_UPDATE} - Represents the stream used for stock update messages.</li>
+ *   <li>{@link #PRODUCT_UPDATE} - Represents the stream used for product update messages.</li>
+ *   <li>{@link #ORDER_UPDATE} - Represents the stream used for order update messages.</li>
+ * </ul>
+ */
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum StreamNames {
