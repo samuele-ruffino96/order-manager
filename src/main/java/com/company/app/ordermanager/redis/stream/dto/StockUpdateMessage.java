f34ee29377084253c863fd1b03fbe559fb6f922e
samuele ruffino <sruffino@sinelec.it>
2025-02-17 15:49:53 +0100

    feat(stock): add orderItemId to StockUpdateMessage
    Updated `StockUpdateMessage` to include `orderItemId` for better granularity in stock operations. This improves inventory tracking accuracy.

--- src/main/java/com/company/app/ordermanager/redis/stream/dto/StockUpdateMessage.java
+++ src/main/java/com/company/app/ordermanager/redis/stream/dto/StockUpdateMessage.java
@@ -6,20 +6,18 @@ import lombok.Data;
 import java.util.UUID;
 
 /**
- * Represents a message used for stock updates such as reserving or canceling stock
- * for a specific product within an order. This class encapsulates the details of
- * the stock update operation.
- * <p>
- * The {@code StockUpdateMessage} includes the order ID, the type of update operation,
- * the product ID, and the quantity of stock being updated. The {@link UpdateType}
- * enumeration defines the available operations: {@code RESERVE} for reserving stock
- * and {@code CANCEL} for reverting a previous reservation.
- * </p>
+ * Represents a stock update message containing information crucial for updating stock levels
+ * in inventory systems. The message includes details such as order identification,
+ * item identification, type of update, product identification, and quantity.
+ *
+ * This class is typically used to communicate stock reservation or cancellation
+ * instructions to inventory systems or services.
  */
 @Data
 @Builder
 public class StockUpdateMessage {
     private UUID orderId;
+    private UUID orderItemId;
     private UpdateType updateType;
     private UUID productId;
     private int quantity;
